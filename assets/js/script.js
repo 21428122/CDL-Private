a030f71a99bab7374c53c9fd8e6c18c5ff5124d8
21428122 <shadowblaze24003@gmail.com>
2023-09-07 22:32:18 +0530

    added title

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,25 +56,12 @@ const backTopBtn = document.querySelector("[data-back-top-btn]");
 
  let lastScrollPos = 0;
 
-// const hideHeader = function () {
-//   const isScrollBottom = lastScrollPos < window.scrollY;
-  // if (isScrollBottom) {
-  //   header.classList.add("hide");
-  // } else {
-  //   header.classList.remove("hide");
-  // }
 
-//   lastScrollPos = window.scrollY;
-// }
 
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 0) {
     header.classList.add("active");
-    // backTopBtn.classList.add("active");
-    // hideHeader();
-  // } else {
-  //   header.classList.remove("active");
-  //   backTopBtn.classList.remove("active");
+ 
    }
 });
 
